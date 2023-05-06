4bb601acd339a2f7e688c1519426e3370078f055
radialcode-mohit <mohitgirdhar9034@gmail.com>
2023-05-06 11:51:13 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+import MyNav from "./MyNav";
+import yellowstar from "../assets/images/svg/yellowstar.svg";
+import whitestar from "../assets/images/svg/whitestar.svg";
+import dogimg from "../assets/images/svg/dog.svg";
+import showerimg from "../assets/images/svg/shower.svg";
+import crosss from "../assets/images/svg/crosss.svg";
+import topdots from "../assets/images/img/rightdots.png";
+import bottomdots from "../assets/images/img/leftdot.png";
+import { Col, Row } from "react-bootstrap";
+const Header = () => {
+  return (
+    <section className="bg_header">
+      <MyNav />
+      <div className="my_container mt-5">
+        <div className="green_div mx-auto position-relative">
+          <img
+            src={topdots}
+            alt=""
+            className=" position-absolute topdots_pos d-none d-lg-block"
+          />
+          <img
+            src={bottomdots}
+            alt=""
+            className=" position-absolute botom_dots d-none d-lg-block"
+          />
+          <div className="blurdiv pt-5 pb-4">
+            <div className="text-center">
+              <h1 className="text-uppercase ff_comic fs_md text-white">
+                Platte River Campground
+              </h1>
+              <p className="mb-0 ff_callibary fw-normal fs_sm clr_fff_opac mt-3 pt-1">
+                Among Top 50 campgrounds in California
+              </p>
+              <div className="d-flex justify-content-center pt-1">
+                {/* <img src={yellowstar} alt="yellowstar" />
+                <img src={yellowstar} alt="yellowstar" />
+                <img src={yellowstar} alt="yellowstar" />
+                <img src={yellowstar} alt="yellowstar" />
+                <img src={whitestar} alt="whitestar" /> */}
+                <span class="rate">
+                  <i>★</i>
+                  <i>★</i>
+                  <i>★</i>
+                  <i>★</i>
+                  <i>★</i>
+                </span>
+              </div>
+              <p className="mb-0 ff_callibary fw-normal fs_sm clr_fff_opac pt-3">
+                5685 Lake Michigan Rd, Honor, MI 49640, United States <br />{" "}
+                Contact: +1 231 326 5134 <br /> www.example.com
+              </p>
+            </div>
+            <div className="max_w_300px mx-auto">
+              {" "}
+              <Row className=" justify-content-center pt-4">
+                <Col xs={6} md={4} className=" mt-4 mt-sm-0">
+                  <div className="px-2 me-md-4 ms-md-2">
+                    {" "}
+                    <div className=" crlces mx-auto  d-flex flex-column justify-content-center align-items-center">
+                      <p className=" ff_callibary fw_700 fs_sm text-white mb-0">
+                        All year
+                      </p>
+                      <p className=" ff_callibary fw_400 fs_sm clr_fff_opac8 mb-0">
+                        Season
+                      </p>
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={6} md={4} className=" mt-4 mt-sm-0">
+                  <div className="px-2 me-md-4 ms-md-2">
+                    {" "}
+                    <div className=" crlces mx-auto  d-flex flex-column justify-content-center align-items-center">
+                      <p className=" ff_callibary fw_700 fs_sm text-white mb-0">
+                        174
+                      </p>
+                      <p className=" ff_callibary fw_400 fs_sm clr_fff_opac8 mb-0">
+                        Total Sites
+                      </p>
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={6} md={4} className=" mt-4 mt-md-0">
+                  <div className="px-2 me-md-4 ms-md-2">
+                    {" "}
+                    <div className=" crlces mx-auto  d-flex flex-column justify-content-center align-items-center">
+                      <p className=" ff_callibary fw_700 fs_sm text-white mb-0">
+                        45
+                      </p>
+                      <p className=" ff_callibary fw_400 fs_sm clr_fff_opac8 mb-0">
+                        Nightly cost
+                      </p>
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={6} md={4} className=" mt-4">
+                  <div className="px-2 me-md-4 ms-md-2">
+                    {" "}
+                    <div className=" crlces mx-auto  d-flex flex-column justify-content-center align-items-center">
+                      <div className=" position-relative">
+                        <img src={dogimg} alt="dog" />
+                        <img
+                          src={crosss}
+                          alt=""
+                          className="cross_pos position-absolute"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={6} md={4} className=" mt-4">
+                  <div className="px-2 me-md-4 ms-md-2">
+                    {" "}
+                    <div className=" crlces mx-auto  d-flex flex-column justify-content-center align-items-center">
+                      <div className=" position-relative">
+                        <img src={showerimg} alt="showerimg" />{" "}
+                        <img
+                          src={crosss}
+                          alt=""
+                          className="cross_pos position-absolute"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={6} md={4} className=" mt-4">
+                  <div className="px-2  me-md-4 ms-md-2">
+                    {" "}
+                    <div className="py-4 crlces mx-auto  d-flex flex-column justify-content-center align-items-center">
+                      <p className=" ff_callibary fw_700 fs_sm text-white mb-0">
+                        RV/Trailers
+                      </p>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-5"></div>
+      <div className="pb-5"></div>
+      {/* <div className="pb-5"></div> */}
+    </section>
+  );
+};
+
+export default Header;
---
+++ src/components/MyNav.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import logo from "../assets/images/svg/logo.svg";
+import close from "../assets/images/img/close.png";
+const MyNav = () => {
+  const [first, setfirst] = useState(false);
+  const [second, setsecond] = useState(0);
+  return (
+    <nav className="bg_green">
+      <div className="my_container">
+        <div className="d-flex justify-content-between align-items-center ">
+          <a href="#" className="me-lg-5 pe-lg-5">
+            <img src={logo} alt="logo" />
+          </a>
+          <ul
+            className={
+              first
+                ? "d-flex mb-0 ps-0 ms-lg-5 ps-lg-5 flex-column flex-lg-row justify-content-center align-items-center mobileiew left_0"
+                : "d-flex mb-0 ps-0 ms-lg-5 ps-lg-5 flex-column flex-lg-row justify-content-center align-items-center mobileiew left_100 "
+            }
+          >
+            <li className=" mt-4 mt-lg-0  white_before ms-lg-5">
+              <a href="#" className="hover_underline ff_comic fs_sm text-white">
+                HOME
+              </a>
+            </li>
+            <li className=" mt-4 mt-lg-0 ms-lg-3 ps-lg-1 white_before2">
+              <a
+                href="#"
+                className="hover_underline2 ff_comic fs_sm text-white text-uppercase"
+              >
+                Campgrounds
+              </a>
+            </li>
+            <li className=" mt-4 mt-lg-0 ms-lg-3 ps-lg-1 white_before3">
+              <a
+                href="#"
+                className="hover_underline3 ff_comic fs_sm text-white"
+              >
+                About us
+              </a>
+            </li>
+            <li className=" mt-4 mt-lg-0 ms-lg-3 ps-lg-1 white_before4">
+              <a
+                href="#"
+                className="hover_underline4 ff_comic fs_sm text-white"
+              >
+                Contact
+              </a>
+            </li>{" "}
+            <button className=" mt-4 mt-lg-0 d-lg-none ff_comic fs_sm text-white bg-transparent br_30 p_10_28 border_2px_green">
+              Sign Up
+            </button>
+            <button className=" mt-4 mt-lg-0 d-lg-none ff_comic fs_sm text-white ms-lg-4 bg_btn_green br_30 p_10_28 border_2px_green ">
+              Log in
+            </button>
+            <img
+              src={close}
+              alt="close"
+              className="d-lg-none position-absolute close_img_pos"
+              onClick={() => {
+                setfirst(false);
+              }}
+            />
+          </ul>
+          <ul className="d-flex btns mb-0 ps-0 d-none d-lg-flex">
+            <button className="hover_bg_btn_green transition ff_comic fs_sm text-white bg-transparent br_30 p_10_28 border_2px_green">
+              Sign Up
+            </button>
+            <button className="hover_bg_btn_green transition  ff_comic fs_sm text-white ms-4 bg_btn_green br_30 p_10_28 border_2px_green ">
+              Log in
+            </button>
+          </ul>
+          <a
+            href="#"
+            className="nav_container d-lg-none"
+            onClick={() => {
+              setfirst(true);
+            }}
+          >
+            <span className="navicon"></span>
+            <span className="navicon mt-2"></span>
+            <span className="navicon mt-2"></span>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default MyNav;
